cbed0e0a823ebbc7776d6e1b53a548604750c5c6
cfi30 <git@cfillion.tk>
2014-05-27 18:38:52 -0400

    objects/image: create manager

--- lib/slidefield/objects/image.rb
+++ lib/slidefield/objects/image.rb
@@ -2,7 +2,34 @@ module SlideField::ObjectRules
   class Image < GBase
     def rules
       variable :source, :string
+      variable :size, :size, [0, 0]
       super
+    end
+  end
+end
+
+module SlideField::ObjectManager
+  class Image < Base
+    def load
+      @x = @obj.get :x
+      @y = @obj.get :y
+      @z = @obj.get :z
+
+      source = File.expand_path @obj.get(:source), @obj.include_path
+      width, height = @obj.get :size
+
+      @image = Gosu::Image.new @window, source, true
+      width = @image.width if 0 == width
+      height = @image.height if 0 == height
+
+      @w_scale = width / @image.width.to_f
+      @h_scale = height / @image.height.to_f
+    end
+
+    def draw
+      @window.scale @w_scale, @h_scale do
+        @image.draw @x, @y, @z
+      end
     end
   end
 end
