95d8479018406b60a021bf5ae4711409dba73222
cfi30 <git@cfillion.tk>
2014-05-27 23:42:06 -0400

    viewer: slide navigation

--- lib/slidefield/viewer.rb
+++ lib/slidefield/viewer.rb
@@ -16,22 +16,57 @@ class SlideField::Viewer < Gosu::Window
     project[:slide].each {|slide_data|
       manager = SlideField::ObjectManager.new slide_data, self
       manager.load
-      manager.activate
       @slides << manager
     }
+
+    change_slide 0
   end
 
   def update
   end
 
   def draw
-    @slides.first.draw
+    scale @x_scale, @y_scale do
+      @current.draw
+    end
   end
 
   def button_down(id)
     case id
+    when Gosu::KbHome
+      change_slide 0
+    when Gosu::KbEnd
+      change_slide @slides.length-1
+    when
+      Gosu::KbReturn,
+      Gosu::KbSpace,
+      Gosu::KbTab,
+      Gosu::KbRight,
+      Gosu::KbDown,
+      Gosu::KbPageDown,
+      Gosu::KbNumpadAdd
+
+      change_slide @index+1
+    when
+      Gosu::KbBackspace,
+      Gosu::KbLeft,
+      Gosu::KbUp,
+      Gosu::KbPageUp,
+      Gosu::KbNumpadSubtract
+
+      change_slide @index-1
     when Gosu::KbEscape, 86, Gosu::KbQ
       close
     end
+  end
+
+  private
+  def change_slide(index)
+    return if index < 0 || index > @slides.length-1
+    @index = index
+
+    @current.deactivate if @current
+    @current = @slides[index]
+    @current.activate
   end
 end
