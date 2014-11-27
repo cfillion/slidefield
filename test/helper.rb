5d7f8b6d622dcf5a081fe117a97c779d14da7e7f
cfillion <cfillion@users.noreply.github.com>
2014-11-27 18:21:08 -0500

    cleanup test helper

--- test/helper.rb
+++ test/helper.rb
@@ -11,19 +11,8 @@ SimpleCov.formatters = [
 SimpleCov.start {
   project_name 'SlideField'
   add_filter '/test/'
-
-  add_group 'Objects', '/lib/slidefield/objects/'
 }
 
 require 'minitest/autorun'
-
-# use awesome_print for objects diff
-module MiniTest
-  module Assertions
-    def mu_pp(obj)
-      obj.awesome_inspect
-    end
-  end
-end
 
 require 'slidefield'
