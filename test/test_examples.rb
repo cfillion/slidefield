c06c7c5ebf4ef11452b39688401a5954a31acfd4
cfillion <cfillion@users.noreply.github.com>
2014-11-27 19:25:34 -0500

    simplify example tests

--- test/test_examples.rb
+++ test/test_examples.rb
@@ -7,28 +7,18 @@ class TestExamples < MiniTest::Test
   end
 
   def test_minimal
-    file_path = File.join(@examples_path, 'minimal/main.sfp')
+    path = File.join @examples_path, 'minimal/main.sfp'
+    @interpreter.run_file path
 
-    @interpreter.run_file file_path
-
-    assert_equal File.dirname(file_path), @interpreter.root.include_path
-    assert_equal File.dirname(file_path), @interpreter.root[:layout].first.include_path
-    assert_equal File.basename(file_path), @interpreter.root.context
-    assert_equal File.basename(file_path), @interpreter.root[:layout].first.context
+    assert_empty @interpreter.diagnostics
+    refute @interpreter.failed?
   end
 
   def test_complete
-    file_path = File.join @examples_path, 'complete/main.sfp'
-    mountains_path = File.join @examples_path, 'complete/slides/mountains.sfi'
+    path = File.join @examples_path, 'complete/main.sfp'
+    @interpreter.run_file path
 
-    @interpreter.run_file file_path
-
-    assert_equal File.dirname(mountains_path), @interpreter.root[:slide].first.include_path
-    assert_equal File.dirname(mountains_path), @interpreter.root[:slide].first.children.first.include_path
-    assert_equal 'slides/mountains.sfi', @interpreter.root[:slide].first.context
-    assert_equal 'slides/mountains.sfi', @interpreter.root[:slide].first.children.first.context
-
-    assert_equal File.dirname(file_path), @interpreter.root.include_path
-    assert_equal 'main.sfp', @interpreter.root.context
+    assert_empty @interpreter.diagnostics
+    refute @interpreter.failed?
   end
 end
