e3f43235799c2c17cffc7ba9d9642e445d21ce62
cfillion <cfillion@users.noreply.github.com>
2015-06-04 15:58:59 -0400

    restart from scratch in C++
    ruby's memory management is inappropriate for this kind of program
    and C++ has access to better graphics libraries

--- lib/slidefield/errors.rb
+++
@@ -1,8 +0,0 @@
-module SlideField
-  Error = Class.new StandardError
-
-  AlreadyAdoptedError    = Class.new Error
-  ColorOutOfBoundsError  = Class.new Error
-  ForeignValueError      = Class.new Error
-  UndefinedObjectError   = Class.new Error
-end
---
+++ src/main.cxx
@@ -0,0 +1,45 @@
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <sys/ioctl.h>
+
+using namespace boost;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  namespace po = program_options;
+
+  const string caption = "SlideField v0.3";
+
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+  po::options_description desc(caption, w.ws_col);
+  desc.add_options()
+    ("help,h", "display this help and exit")
+    ("version,v", "output version information and exit")
+  ;
+
+  po::variables_map opts;
+
+  try {
+    po::store(po::parse_command_line(argc, argv, desc), opts);
+    po::notify(opts);
+  }
+  catch(po::error &err) {
+    cerr << err.what() << endl;
+    return EXIT_FAILURE;
+  }
+
+  if(opts.count("help")) {
+    cout << desc;
+    return EXIT_SUCCESS;
+  }
+
+  if(opts.count("version")) {
+    cout << caption << endl;
+    return EXIT_SUCCESS;
+  }
+
+  return EXIT_SUCCESS;
+}
